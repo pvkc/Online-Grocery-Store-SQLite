34c6ba69540ab4952cc3fbdc7f49109874ab9656
Venkata Krishna Chaitanya Polavarapu <vpolavarapu1@hawk.iit.edu>
2017-05-21 06:47:57 +0000

    Support for suggesstions added to index.js

--- OnlineGroceryStore/store/static/js/index.js
+++ OnlineGroceryStore/store/static/js/index.js
@@ -1,13 +1,57 @@
-$('#searchProduct').click( function (e) {
+// using jQuery, get the csrf token
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
 
-    if ($('#searchText').val() == ''){
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+$('#searchText').keydown(
+    function (e) {
+        $.ajax({
+            url:'autoFillSearch',
+            type: 'POST',
+            data: {'searchText': $('#searchText').val(),
+                    'state': $('#storeState').data('state')},
+            success: function (searchList) {
+                $('#searchText').typeahead({source:searchList['resp']});
+            }
+        })
+    }
+);
+$('#searchProduct').click(function (e) {
+
+    if ($('#searchText').val() === '') {
         return false;
     }
 
     e.preventDefault();
 
 
-    formData = { 'searchText':$('#searchText').val(),
+    formData = {
+        'searchText': $('#searchText').val(),
         'state': $('#storeState').data('state')
     };
 
