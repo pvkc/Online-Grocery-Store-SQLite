068f173641bc8c54feafc61cbb930a54cfe646c8
Venkata Krishna Chaitanya Polavarapu <vpolavarapu1@hawk.iit.edu>
2017-05-21 20:09:53 +0000

    Updated settings for local run

--- OnlineGroceryStore/OnlineGroceryStore/settings.py
+++ OnlineGroceryStore/OnlineGroceryStore/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['KEY']
+
+SECRET_KEY = os.environ['KEY'] if os.environ.get('KEY',[]) else 'fhuihsf9819*_+fsifa873287gnvzbqcxvlkcasc' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = False
-ALLOWED_HOSTS = [os.environ['IP']]
+if os.environ.get('KEY',[]) == [] :
+	DEBUG = True
+else:
+	DEBUG = False
+ALLOWED_HOSTS = [os.environ['IP']] if os.environ.get('IP',[]) else ['*']
 
 
 # Application definition
